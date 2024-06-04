1146f845f478730da25e3d44b68a36362a014b46
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-05 00:19:17 +0900

    project init

---
+++ src/main/java/org/jungle/code_post/CodePostApplication.java
@@ -0,0 +1,13 @@
+package org.jungle.code_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jungle/code_post/CodePostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jungle.code_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
