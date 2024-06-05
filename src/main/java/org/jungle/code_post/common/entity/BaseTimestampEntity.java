3f6250a080cdfc6e7ca52e1990ca2c72fbdc3495
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-05 17:25:07 +0900

    feat: Post Entity 작성 완료 (#3) - Post 구현 - BaseTimestampEntity 구현 - spring boot 시작 클래스에 @EnableJpaAuditing 어노테이션 추가

---
+++ src/main/java/org/jungle/code_post/common/entity/BaseTimestampEntity.java
@@ -0,0 +1,26 @@
+package org.jungle.code_post.common.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimestampEntity {
+    @CreatedDate
+    @Column(name = "created_at",updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
