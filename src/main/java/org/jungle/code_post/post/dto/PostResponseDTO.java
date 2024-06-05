ee40877e22d76d2a4c4343187c0385c858355d9d
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-05 19:38:09 +0900

    feat: PostResponseDTO 및 ApiResponseDTO 구현 (#5) - 컨트롤러에서 DTO 반환 시 ResponseHandler를 통해 ApiResponseDTO로 감싸서 반환하도록 구현

---
+++ src/main/java/org/jungle/code_post/post/dto/PostResponseDTO.java
@@ -0,0 +1,35 @@
+package org.jungle.code_post.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.jungle.code_post.post.entity.Post;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class PostResponseDTO {
+    private Long id;
+    private String title;
+    private String link;
+    private String category;
+    private Integer score;
+    private String author;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public static PostResponseDTO of(Post post) {
+        return PostResponseDTO.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .link(post.getLink())
+                .category(post.getCategory())
+                .score(post.getScore())
+                .author(post.getAuthor())
+                .created_at(post.getCreatedAt())
+                .updated_at(post.getUpdatedAt())
+                .build();
+    }
+}
