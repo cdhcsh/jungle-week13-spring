99aa6a4d699a1626ec7f0085e4040c3e4b8a4958
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-06 23:35:10 +0900

    feat: Member,MemberRepository 구현 (#11) - Member : 멤버 entity - MemberRepository : 멤버 repository - MemberRepositoryTest : 삽입/조회 테스트

---
+++ src/main/java/org/jungle/code_post/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package org.jungle.code_post.member.repository;
+
+import org.jungle.code_post.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+    public Optional<Member> findByUsername(String username);
+}
