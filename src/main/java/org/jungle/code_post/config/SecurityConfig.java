9d4b71bd582b7623f3dc9cb0517b5223185f72aa
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-08 21:27:18 +0900

    feat: Spring security 설정 (#15) - build.gradle : spring security 의존성 추가 - SecurityConfig : jwt를 위한 설정

---
+++ src/main/java/org/jungle/code_post/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.jungle.code_post.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(CsrfConfigurer::disable);
+        http.cors(Customizer.withDefaults());
+
+        http.sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.formLogin(FormLoginConfigurer::disable);
+        http.httpBasic(HttpBasicConfigurer::disable);
+
+        http.authorizeHttpRequests((auth)-> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
