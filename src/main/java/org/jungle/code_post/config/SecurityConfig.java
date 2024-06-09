6a6aa202a460b49b6a4b2441362e8c42752f53be
donghwan.best <75022174+cdhcsh@users.noreply.github.com>
2024-06-09 21:57:52 +0900

    feat : cors 관련 설정

--- src/main/java/org/jungle/code_post/config/SecurityConfig.java
+++ src/main/java/org/jungle/code_post/config/SecurityConfig.java
@@ -4,17 +4,14 @@ package org.jungle.code_post.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -29,7 +26,18 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(CsrfConfigurer::disable);
+        //noinspection removal
+        http
+                .csrf().disable()
+                .cors().disable()
+                        .sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .formLogin().disable()
+                        .httpBasic().disable()
+                        .authorizeHttpRequests()
+                                .anyRequest().permitAll()
+                        .and()
+                                .build();
+
 
         http.sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
